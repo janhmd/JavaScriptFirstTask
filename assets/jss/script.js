1739d70740b982c0b8fb84e90a76f58bee552724
janhmd <can3hm3d@mail.ru>
2026-07-09 04:27:57 +0400

    Finish task

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const arr = [10,15,25,35]
+const arr = [10,17,26,37]
 
 const students = [
 {
@@ -45,18 +45,207 @@ const students = [
 //         }
 //     }
 //      return result;
-// }
+// };
 
 // console.log(oddNumbers(2,7));
 
-function sumArr(arr){
-    let sum = 0;
-    for(let item of arr){
-        sum += item
-    }
-     return sum;
-}
 
-console.log(sumArr(arr));
+
+
+// function getSum(arr){
+//     let sum = 0;
+//     for(let item of arr){
+//         sum += item
+//     }
+//      return sum;
+// }
+
+// console.log(getSum(arr));
+
+// const getSum = () => {
+//     let sum = 0;
+//     for(let item of arr) {
+//         sum += item;
+//     }
+//     return sum;
+// };
+// console.log(getSum(arr));
+
+
+
+
+// function countEven(arr){
+//     let count = 0;
+//     for(let item of arr){
+//         if(item % 2 === 0){
+//             count++;
+//         }
+        
+//     }
+//     return count;
+// }
+
+// console.log(countEven(arr));
+
+// const countEven = () =>{
+//     let count = 0;
+//     for(let item of arr) {
+//         if(item % 2 === 0){
+//             count++;
+//         }
+//     }
+//     return count;
+// };
+
+
+// console.log(countEven(arr));
+
+
+
+
+// function findFac(n) {
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+// console.log(findFac(7));
+
+// const findFac = (n) => {
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= i;
+//     }
+//     return result;
+// };
+
+// console.log(findFac(7));
+
+
+
+// function isPrime(n) {
+//     if (n <= 1) return false;
+
+//     for (let i = 2; i < n; i++) {
+//         if (n % i === 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// console.log(isPrime(8));
+
+
+// const isPrime = (n) => {
+//     if (n <= 1) return false;
+
+//     for (let i = 2; i < n; i++) {
+//         if (n % i === 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
+// console.log(isPrime(8));
+
+
+
+// function countSpecial(arr) {
+//     let count = 0;
+//     for (let item of arr) {
+//         if (item % 6 > 4) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(countSpecial(arr));
+
+// const countSpecial = (arr) => {
+//     let count = 0;
+//     for (let item of arr) {
+//         if (item % 6 > 4) {
+//             count++;
+//         }
+//     }
+//     return count;
+// };
+
+// console.log(countSpecial(arr));
+
+
+
+// function sumAges(students) {
+//     let sum = 0;
+//     for (let s of students) {
+//         sum += s.age;
+//     }
+//     return sum;
+// }
+
+// console.log(sumAges(students));
+
+
+// const sumAges = (students) => {
+//     let sum = 0;
+//     for (let s of students) {
+//         sum += s.age;
+//     }
+//     return sum;
+// };
+
+// console.log(sumAges(students));
+
+
+// function avgAges(students) {
+//     let sum = 0;
+//     for (let s of students) {
+//         sum += s.age;
+//     }
+//     return sum / students.length;
+// }
+
+// console.log(avgAges(students));
+
+
+
+// const avgAges = (students) => {
+//     let sum = 0;
+//     for (let s of students) {
+//         sum += s.age;
+//     }
+//     return sum / students.length;
+// };
+
+// console.log(avgAges(students));
+
+
+
+
+// function multiplyCountAndSum(students) {
+//     let sum = 0;
+//     for (let s of students) {
+//         sum += s.age;
+//     }
+//     return students.length * sum;
+// }
+
+// console.log(multiplyCountAndSum(students));
+
+
+
+// const multiplyCountAndSum = (students) => {
+//     let sum = 0;
+//     for (let s of students) {
+//         sum += s.age;
+//     }
+//     return students.length * sum;
+// };
+
+// console.log(multiplyCountAndSum(students));
 
 
